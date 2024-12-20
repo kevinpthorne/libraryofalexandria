97ca6f662f06b08d9b0f958c3dae1123a20aa3a4
Kevin Thorne <kevinpthorne@gmail.com>
2024-12-20 00:14:00 -0500

    feat(iam): deployment key

--- libraryofalexandria/iam.nix
+++ libraryofalexandria/iam.nix
@@ -1,7 +1,10 @@
 {
     kevint = {
         host = {  # applied to every library of alexandria host
-            openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz9z1zkXXO45SjKKbryrXZip/HEvZSAV2D/WpygFSFK kevint@Laptop4.local" ];
+            openssh.authorizedKeys.keys = [ 
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz9z1zkXXO45SjKKbryrXZip/HEvZSAV2D/WpygFSFK kevint@Laptop4.local" 
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICAa6gt+RvDM5hDn+GBmWnCaPo3KB6RNdG3so0q3Z8kw kevint@Laptop4.local deployment"
+            ];
         };
     };
 
