cccdb2c7f8d5fdda565e1c838a6bf1f92e6cebf2
Kevin Thorne <kevinpthorne@gmail.com>
2024-12-31 14:22:10 -0500

    fix: updates bootstrap k cluster script

--- bootstrap-cluster-k.sh
+++ bootstrap-cluster-k.sh
@@ -3,7 +3,7 @@ set -xeu
 # set -o pipefail
 
 nix build '.#nixosConfigurations.libraryofalexandria-k-master0-rpi.config.system.build.sdImage' --show-trace
-nix build '.#nixosConfigurations.libraryofalexandria-k-master1-rpi.config.system.build.sdImage' --show-trace
 nix build '.#nixosConfigurations.libraryofalexandria-k-worker0-rpi.config.system.build.sdImage' --show-trace
 nix build '.#nixosConfigurations.libraryofalexandria-k-worker1-rpi.config.system.build.sdImage' --show-trace
 nix build '.#nixosConfigurations.libraryofalexandria-k-worker2-rpi.config.system.build.sdImage' --show-trace
+nix build '.#nixosConfigurations.libraryofalexandria-k-worker3-rpi.config.system.build.sdImage' --show-trace
