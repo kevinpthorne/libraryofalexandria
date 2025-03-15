dd1c8104442527f925135b781b15b9eca76071b8
Kevin Thorne <kevinpthorne@gmail.com>
2025-03-15 00:29:21 -0400

    feat: vm image compression

--- modules/platforms/vm.nix
+++ modules/platforms/vm.nix
@@ -10,11 +10,11 @@
     options = {
         vmHostPlatform = lib.mkOption {
             default = "x86_64-linux";
-            type = lib.types.enum [ "x86_64-linux" "aarch64-linux" ];
+            type = lib.types.enum [ "x86_64-linux" "aarch64-linux" ];  # TODO hook to eachArch from flake input
         };
         vmImage = {
             name = lib.mkOption {
-                default = "${config.vmImage.baseName}-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.raw";
+                default = "${config.vmImage.baseName}-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.qcow2";
                 description = "Name of the generated image file.";
                 type = lib.types.str;
             };
@@ -24,10 +24,18 @@
                 description = "Prefix of the name of the generated image file.";
                 type = lib.types.str;
             };
+
+            compress = lib.mkOption {
+                default = true;
+                description = "Whether to compress the VM image";
+                type = lib.types.bool;
+            };
         };
     };
 
-    config = {
+    config = let 
+        compressArgs = if config.vmImage.compress then "-c" else "";
+    in {
         libraryofalexandria.node.platform = "vm";
         nixpkgs.hostPlatform = lib.mkDefault config.vmHostPlatform;
 
@@ -36,15 +44,19 @@
 
         vmImage.baseName = config.networking.hostName;
         system.builder = {
-            package = pkgs.callPackage ({ stdenv }:
+            package = pkgs.callPackage ({ stdenv, qemu }:
                 stdenv.mkDerivation {
                     name = config.vmImage.name;
                     src = ./.;
+
+                    buildInputs = [ qemu ];
 
                     buildPhase = ''
                         mkdir -p $out/raw-image
                         cd $out/raw-image
                         ${config.system.build.diskoImagesScript} --build-memory 4096
+                        ${pkgs.qemu}/bin/qemu-img convert -p -f raw -O qcow2 ${compressArgs} main.raw main.qcow2
+                        rm main.raw
                     '';
                     # https://github.com/nix-community/disko/blob/v1.11.0/docs/disko-images.md
                     # nix build .#nixosConfigurations.myhost.config.system.build.diskoImagesScript
