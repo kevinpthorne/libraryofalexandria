2be4acf45cbd2c2e5185847fd1f3e7a8da4b75e2
Kevin Thorne <kevinpthorne@gmail.com>
2025-04-04 11:55:07 -0400

    restore colmena config defaults

--- modules/submodules/deployment/colmena.nix
+++ modules/submodules/deployment/colmena.nix
@@ -27,13 +27,13 @@
             openssh.authorizedKeys.keys = colmenaConfig.authorizedKeys;
         };
         deployment = {
-            # tags = [
-            #     "cluster=${config.libraryofalexandria.node.clusterName}"
-            #     "type=${config.libraryofalexandria.node.type}"
-            # ];
-            targetHost = ""; #config.libraryofalexandria.node.hostname;
+            tags = [
+                "cluster=${config.libraryofalexandria.node.clusterName}"
+                "type=${config.libraryofalexandria.node.type}"
+            ];
+            targetHost = config.libraryofalexandria.node.hostname;
             targetPort = 22;
-            targetUser = "colmena"; #colmenaConfig.userName;
+            targetUser = colmenaConfig.userName;
         };
         services.openssh.enable = true;
         security.sudo.extraRules = [   # colmena currently requires non-interactive auth
