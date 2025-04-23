01fc9cdbd1e63e819ec4edb64fc860c6ffb742fe
Kevin Thorne <kevinpthorne@gmail.com>
2025-04-22 22:51:48 -0400

    feat: naive manifest application

---
+++ modules/submodules/k8s/manifests.nix
@@ -0,0 +1,38 @@
+{ pkgs, config, lib, inputs, ... }:
+{
+    imports = [];
+
+    options = {
+        libraryofalexandria.manifests = {
+            enable = lib.mkEnableOption "Apply manifests on this cluster";
+            manifestsToApply = lib.mkOption {
+                type = lib.types.listOf lib.types.attrset;
+            };
+        };
+    };
+
+    config = let
+        package = pkgs.runCommand "render-k8s-manifest" {
+            json = builtins.toJSON {
+                apiVersion = "v1";
+                kind = "List";
+                items = config.libraryofalexandria.manifests.manifestsToApply;
+            };
+            passAsFile = [ "json" ]; # will be available as `$jsonPath`
+        } ''
+            mkdir -p $out
+            cat "$jsonPath" > $out/manifest.json
+        '';
+    in
+    lib.mkIf config.libraryofalexandria.manifests.enable {
+        warnings = [''Only use this if helm charts are somehow unsuitable for the task.''];
+
+        systemd.services.manifest-installer = {
+            wantedBy = [ "kubernetes.service" ];
+            after = [ "kubernetes.service" ];
+            script = ''
+                ${pkgs.kubectl}/bin/kubectl apply -f ${chart.config.package}/manifest.json
+            '';
+        };
+    };
+}
