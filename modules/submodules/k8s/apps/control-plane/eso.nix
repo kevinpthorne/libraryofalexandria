1be23cf71e963b4f5db8860142778be65352a2e9
Kevin Thorne <kevinpthorne@gmail.com>
2025-12-30 18:08:35 -0500

    feat: secret replicator helm

--- modules/submodules/k8s/apps/control-plane/eso.nix
+++ modules/submodules/k8s/apps/control-plane/eso.nix
@@ -1,34 +1,45 @@
 #repo https://charts.external-secrets.io/
 # chart external-secrets-operator
-{ lib, lib2, config, ... }:
+{ lib, lib2, config, pkgs, ... }:
 {
     imports = [ ../../helm-charts.nix ];
 
     config = lib.mkIf config.libraryofalexandria.apps.external-secrets-operator.enable {
         libraryofalexandria.helmCharts.enable = true;
-        libraryofalexandria.helmCharts.charts = [{
-            name = "external-secrets";
-            chart = "external-secrets/external-secrets";
-            version = config.libraryofalexandria.apps.external-secrets-operator.version;
-            values = lib2.deepMerge [{
-                installCRDs = true;
+        libraryofalexandria.helmCharts.charts = [
+            {
+                name = "external-secrets";
+                chart = "external-secrets/external-secrets";
+                version = config.libraryofalexandria.apps.external-secrets-operator.version;
+                values = lib2.deepMerge [{
+                    installCRDs = true;
+                    crds.createClusterSecretStore = true;  # for cross-namespace access
 
-                global.securityContext = {
-                    runAsUser = 1000;
-                    runAsGroup = 1000;
-                    fsGroup = 1000;
-                };
+                    global.securityContext = {
+                        runAsUser = 1000;
+                        runAsGroup = 1000;
+                        fsGroup = 1000;
+                    };
 
-                commonSecurityContext = {
-                    allowPrivilegeEscalation = false;
-                    capabilities.drop = [ "ALL" ];
-                    readOnlyRootFilesystem = true;
-                    runAsNonRoot = true;
-                    seccompProfile.type = "RuntimeDefault";
+                    commonSecurityContext = {
+                        allowPrivilegeEscalation = false;
+                        capabilities.drop = [ "ALL" ];
+                        readOnlyRootFilesystem = true;
+                        runAsNonRoot = true;
+                        seccompProfile.type = "RuntimeDefault";
+                    };
+                } config.libraryofalexandria.apps.external-secrets-operator.values];
+                namespace = "external-secrets";
+                repo = "https://charts.external-secrets.io";
+            }
+            {
+                name = "secret-replicator";
+                chart = "${pkgs.secret-replicator-helm}";
+                values = {
+                    cluster = config.libraryofalexandria.cluster;
                 };
-            } config.libraryofalexandria.apps.external-secrets-operator.values];
-            namespace = "external-secrets";
-            repo = "https://charts.external-secrets.io";
-        }];
+                namespace = "external-secrets";
+            }
+        ];
     };
 }
