224724f13d1352fa97acbb9c8d520696f07fd3ef
Kevin Thorne <kevinpthorne@gmail.com>
2025-12-30 13:13:02 -0500

    feat: external secrets operator

---
+++ modules/submodules/k8s/apps/control-plane/eso.nix
@@ -0,0 +1,34 @@
+#repo https://charts.external-secrets.io/
+# chart external-secrets-operator
+{ lib, lib2, config, ... }:
+{
+    imports = [ ../../helm-charts.nix ];
+
+    config = lib.mkIf config.libraryofalexandria.apps.external-secrets-operator.enable {
+        libraryofalexandria.helmCharts.enable = true;
+        libraryofalexandria.helmCharts.charts = [{
+            name = "external-secrets";
+            chart = "external-secrets/external-secrets";
+            version = config.libraryofalexandria.apps.external-secrets-operator.version;
+            values = lib2.deepMerge [{
+                installCRDs = true;
+
+                global.securityContext = {
+                    runAsUser = 1000;
+                    runAsGroup = 1000;
+                    fsGroup = 1000;
+                };
+
+                commonSecurityContext = {
+                    allowPrivilegeEscalation = false;
+                    capabilities.drop = [ "ALL" ];
+                    readOnlyRootFilesystem = true;
+                    runAsNonRoot = true;
+                    seccompProfile.type = "RuntimeDefault";
+                };
+            } config.libraryofalexandria.apps.external-secrets-operator.values];
+            namespace = "external-secrets";
+            repo = "https://charts.external-secrets.io";
+        }];
+    };
+}
