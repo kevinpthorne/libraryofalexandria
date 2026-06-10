e5da3bc2f2e09e407afe75346da0289a5ad003ef
Kevin Thorne <kevinpthorne@gmail.com>
2026-06-09 23:29:54 -0400

    wip: more kubeapi server mem optimization

--- modules/submodules/k8s/engines/rke2/resource-res.nix
+++ modules/submodules/k8s/engines/rke2/resource-res.nix
@@ -1,13 +1,17 @@
 { config, lib, ... }:
+let
+  isRke2 = config.libraryofalexandria.cluster.k8sEngine == "rke2";
+  isMaster = config.libraryofalexandria.node.type == "master";
+in
 {
   environment.etc."rancher/rke2/config.yaml.d/99-protect-node.yaml".text =
-    lib.mkIf (config.libraryofalexandria.cluster.k8sEngine == "rke2")
+    lib.mkIf isRke2
       (
-        if (config.libraryofalexandria.node.type == "master") then
+        if isMaster then
           ''
             # 2. Hard limits for static pods
             control-plane-resource-limits:
-              - kube-apiserver-memory=3072Mi
+              - kube-apiserver-memory=2560Mi
               - etcd-memory=1500Mi
               - kube-controller-manager-memory=512Mi
 
@@ -15,11 +19,15 @@
             etcd-arg:
               # Lower the DB quota (default is 2G). 1G is plenty for 5 nodes.
               - "quota-backend-bytes=1073741824"  # TODO scale per node count
+            kube-apiserver-extra-env:
+              - "GOMEMLIMIT=2048MiB"
             kube-apiserver-arg:
               # Throttle read requests (LIST/WATCH from ArgoCD/Longhorn)
               - "max-requests-inflight=150"     # Default is 400
               # Throttle write requests (POST/PUT)
               - "max-mutating-requests-inflight=50" # Default is 200
+              # Reduce default watch cache size for all resources (primarily CRDs)
+              - "default-watch-cache-size=50"
 
             # 4. OS and Kubelet Protection
             kubelet-arg:
@@ -36,4 +44,16 @@
               - "pod-max-pids=1024"
           ''
       );
+
+  systemd.services = lib.mkIf isRke2 (
+    if isMaster then {
+      rke2-server.restartTriggers = [
+        config.environment.etc."rancher/rke2/config.yaml.d/99-protect-node.yaml".source
+      ];
+    } else {
+      rke2-agent.restartTriggers = [
+        config.environment.etc."rancher/rke2/config.yaml.d/99-protect-node.yaml".source
+      ];
+    }
+  );
 }
