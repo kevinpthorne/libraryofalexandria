fa652145903f542b347d75d1243c55ed7b3e5bda
Kevin Thorne <kevinpthorne@gmail.com>
2026-06-18 00:04:16 -0400

    wip: keydb

--- pkgs/keydb/default.nix
+++ pkgs/keydb/default.nix
@@ -35,6 +35,18 @@ stdenv.mkDerivation rec {
     patchShebangs deps/
     substituteInPlace deps/Makefile \
       --replace "--disable-cxx" "--disable-cxx --with-lg-page=16"
+
+    # Fix linuxMadvFreeForkBugCheck assertion failure on kernels with page sizes != 4096
+    # (e.g. ARM64 with 64k pages). Upstream issue: https://github.com/Snapchat/KeyDB/issues/894
+    substituteInPlace src/server.cpp \
+      --replace "const long map_size = 3 * 4096;" \
+                "unsigned long page_size = sysconf(_SC_PAGESIZE); const long map_size = 3 * page_size;" \
+      --replace "q = p + 4096;" \
+                "q = p + page_size;" \
+      --replace "ret = mprotect(q, 4096, PROT_READ | PROT_WRITE);" \
+                "ret = mprotect(q, page_size, PROT_READ | PROT_WRITE);" \
+      --replace "ret = madvise(q, 4096, MADV_FREE);" \
+                "ret = madvise(q, page_size, MADV_FREE);"
   '';
 
   dontConfigure = true;
