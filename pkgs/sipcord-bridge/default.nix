e2a7234e71817045590e2f0914c09a677c7dd76e
Kevin Thorne <kevinpthorne@gmail.com>
2026-07-28 21:24:52 -0400

    fix: sipcord image src hash

--- pkgs/sipcord-bridge/default.nix
+++ pkgs/sipcord-bridge/default.nix
@@ -18,7 +18,7 @@ rustPlatform.buildRustPackage rec {
     owner = "coral";
     repo = "sipcord-bridge";
     rev = "1ff12ecdff103b32e01dfd80e845d2397152f466";
-    sha256 = "sha256-4c4hlyS/d1+L4K+vXzQpYV5zO1S9+9Nl5NqW1Nq1Nq0="; # Placeholder, will update on build
+    sha256 = "sha256-QVC2cbyM1vkLA+/IKBMO2uzvK7N4JfelbP4xvn3+9Xk=";
   };
 
   cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Placeholder, will update on build
